1acfe56d8ca50fdfc947923e2cf4e293e1ea7b4a
Travis Colbert <trav.colbert@gmail.com>
2017-04-25 14:43:20 -0700

    removed extra position function. initializes fully

--- laminar.scroll.js
+++ laminar.scroll.js
@@ -6,7 +6,6 @@
 
 var Laminar = Laminar || {};
 
-
 /* General Idea: Make a list of all the elements that need to be aware of their
  * vertical positioning.
  * For each element, list the upper- and lower-range where an event should
@@ -28,12 +27,16 @@ Laminar.Scroll = (function() {
   }
   Scroll.prototype.init = function(scrollObj) {
     var that = this;
+    window.addEventListener('scroll',function(e) {
+      that.scrollEvent();
+    });
     this.scrollCheckLoop = setInterval(function() {
       if(that.scrollOccured) {
         that.checkBodyPosition();
         that.scrollOccured = false;
       }
     }, this.scrollTimeout);
+    this.scrollOccured = true;
     return this;
   };
   Scroll.prototype.stop = function() {
@@ -44,24 +47,19 @@ Laminar.Scroll = (function() {
     this.scrollOccured = true;
     return this;
   };
-  Scroll.prototype.checkElementPosition = function(element,conditions) {
-    for(var c=0;c<conditions.length;c++) {
-      if(conditions[c].testFunc(element)) {
-        conditions[c].trueCallback(element);
-      } else {
-        conditions[c].falseCallback(element);
-      }
-    }
-  };
   Scroll.prototype.checkBodyPosition = function() {
     for(var i=0;i<this.elements.length;i++) {
       var element = this.elements[i];
-      var domElement = (element.elementSelector instanceof Laminar.Widget) ? element.elementSelector : document.querySelector(element.elementSelector);
+      if(Laminar.Widget && (element.elementSelector instanceof Laminar.Widget)) {
+        var domElement = element.elementSelector;
+      } else {
+        var domElement = document.querySelector(element.elementSelector);
+      }
       for(var c=0;c<element.conditions.length;c++) {
         if(element.conditions[c].testFunc(domElement)) {
-          element.conditions[c].trueCallback(domElement);
+          if(element.conditions[c].hasOwnProperty("trueCallback")) element.conditions[c].trueCallback(domElement);
         } else {
-          element.conditions[c].falseCallback(domElement);
+          if(element.conditions[c].hasOwnProperty("falseCallback")) element.conditions[c].falseCallback(domElement);
         }
       }
     }
